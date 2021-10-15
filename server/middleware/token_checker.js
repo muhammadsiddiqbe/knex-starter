127393c5a1049e82cbec27484398706dd3e31f8a
muhammadsiddiq <muhammadsiddiq@abutech.uz>
2021-10-15 14:20:13 +0500

    complete login

--- server/middleware/token_checker.js
+++ server/middleware/token_checker.js
@@ -2,9 +2,12 @@ const { verify } = require("../utils/jwt");
 
 module.exports = async function (req, res, next) {
   const token = req.cookies.token;
-  const user = await verify(token);
 
-  req.user = user;
+  if (token) {
+    const user = await verify(token);
+
+    req.user = user;
+  }
 
   next();
 };
