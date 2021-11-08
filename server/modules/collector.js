c430aedbbd4c26d886ad31fccc35f92e0e990a3b
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2021-11-08 10:16:34 +0500

    complete model collector

--- server/modules/collector.js
+++ server/modules/collector.js
@@ -2,15 +2,20 @@
 
 const fs = require("fs");
 const path = require("path");
-const knex = require("../config/database");
+const knex = require("../../config/database");
 
 const getModelFiles = (dir) =>
   fs
     .readdirSync(dir)
-    .filter((file) => file.indexOf(".") !== 0 && file !== "index.js")
+    .filter(
+      (file) =>
+        file.indexOf(".") !== 0 &&
+        file !== "collector.js" &&
+        file !== "routes.js"
+    )
     .map((file) => path.join(dir, file));
 
-const files = getModelFiles(__dirname + "/modules");
+const files = getModelFiles(__dirname);
 
 const models = files.reduce((modelsObj, filename) => {
   const initModel = require(filename + "/model.js");
