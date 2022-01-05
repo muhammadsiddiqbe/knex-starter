8f349cb64236743f7fb2e7a19e8eba47d209d51f
muhammadsiddiqbe@gmail.com <muhammadsiddiqbe@gmail.com>
2022-01-05 11:17:12 +0500

    fix typo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = require("./server");
 
 app
   .listen(PORT, () => {
-    console.info(`Server started on http://loczalhost:${PORT}`);
+    console.info(`Server started on http://127.0.0.1:${PORT}`);
   })
   .on("error", (err) => {
     console.error("ERROR: ", err);
